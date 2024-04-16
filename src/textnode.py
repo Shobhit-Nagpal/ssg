b31431fcecfd2f86a041c0f1196c746b9b3bc061
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-04-16 21:31:46 +0530

    add unit tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,11 +1,11 @@
 class TextNode: 
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, txtNode):
-        if self.text == txtNode.text and self.text_type == txtNode.text_type and self.text == txtNode.url:
+        if self.text == txtNode.text and self.text_type == txtNode.text_type and self.url == txtNode.url:
             return True
         return False
 
