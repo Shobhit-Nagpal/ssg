7bf987ee0cc93397f8d78b9cf74958a4cdacd4d4
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-04-20 21:04:21 +0530

    add spliting with delimiter and unit tests

---
+++ src/convert.py
@@ -0,0 +1,50 @@
+from htmlnode import LeafNode
+from textnode import TextNode
+
+TEXT_TYPE_TEXT = "text"
+TEXT_TYPE_BOLD = "bold"
+TEXT_TYPE_ITALIC = "italic"
+TEXT_TYPE_CODE = "code"
+TEXT_TYPE_LINK = "link"
+TEXT_TYPE_IMAGE = "image"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(value=text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Not a valid text node type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    nodes = []      
+    closing_limiter_found = False
+
+    for node in old_nodes:
+        if node.text_type != TEXT_TYPE_TEXT:
+            nodes.append(node)
+            continue
+        
+        if node.text.count(delimiter) % 2 != 0:
+            raise Exception("Invalid Markdown syntax")
+
+        split_nodes = node.text.split(delimiter)
+        for idx, split_node in enumerate(split_nodes):
+            if idx % 2 == 0:
+                text_node = TextNode(split_node, TEXT_TYPE_TEXT)
+                nodes.append(text_node)
+                continue
+
+            text_node = TextNode(split_node, text_type)
+            nodes.append(text_node)
+
+    return nodes
---
+++ src/test_split_delimiter.py
@@ -0,0 +1,43 @@
+import unittest
+
+from convert import split_nodes_delimiter
+from textnode import TextNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_code_block_split(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        result = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" word", text_type_text),
+        ]
+        self.assertEqual(new_nodes, result)
+
+    def test_italic_split(self):
+        node = TextNode("This is text with a *italic* word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
+        result = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word", text_type_text),
+        ]
+        self.assertEqual(new_nodes, result)
+
+    def test_bold_split(self):
+        node = TextNode("This is text with a **bold** word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        result = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("bold", text_type_bold),
+            TextNode(" word", text_type_text),
+        ]
+        self.assertEqual(new_nodes, result)
+
